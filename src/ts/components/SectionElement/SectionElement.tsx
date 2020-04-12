55293dcc1da2e13f8a2b23a701c518081f2dd2f8
Ricardo Fernandez <ricardo.fernandez@creativedrive.com>
2020-04-11 18:31:29 -0600

    reorganized comp folder structure

--- src/ts/components/SectionElement/SectionElement.tsx
+++ src/ts/components/SectionElement/SectionElement.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement } from 'react';
 import { ISectionElement } from 'interfaces';
-import SectionTitle from './SectionTitle';
-import SectionParagraph from './SectionParagraph';
-import SectionImage from './SectionImage';
+import SectionTitle from 'components/SectionTitle';
+import SectionParagraph from 'components/SectionParagraph';
+import SectionImage from 'components/SectionImage';
 
 const SectionElement = ({ type, text, variants, url, alt }: ISectionElement): ReactElement => {
   let Element = null;
