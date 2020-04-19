989323354829fabe1aeb586811e7e0483886f501
Ricardo Fernandez <ricardo.fernandez@creativedrive.com>
2020-04-19 13:47:49 -0600

    added real validation rule messages

--- src/ts/shared/validationRules.ts
+++ src/ts/shared/validationRules.ts
@@ -3,25 +3,25 @@ import { Validate } from 'react-hook-form';
 
 const validate = (rules: IRules): Validate => (data) => {
   if (rules) {
-    console.log(rules);
     if (data) {
-      console.log(data);
       if (typeof data === 'string') {
-        if (rules.maxLength && data.length > rules.maxLength) return `Exceeded max length`;
-        if (rules.minLength && data.length < rules.minLength) return `blabla add more chars`;
+        if (rules.maxLength && data.length > rules.maxLength)
+          return `Máximo ${rules.maxLength} caractéres`;
+        if (rules.minLength && data.length < rules.minLength)
+          return `Mínimo ${rules.minLength} caractéres`;
       }
       if (data instanceof FileList) {
         if (data[0]) {
           const file = data[0];
           const ext = file.name.split('.').reverse()[0];
-          if (rules.fileType && ext !== rules.fileType) return `Wrong file type`;
-          if (rules.size && file.size > rules.size) return `File is too big`;
+          if (rules.fileType && ext !== rules.fileType) return `Archivo debe ser de tipo ${ext}`;
+          if (rules.size && file.size > rules.size) return `Máximo tamaño de archivo ${rules.size}`;
         } else {
-          if (rules.required) return 'Es necesario adjuntar el archvivo';
+          if (rules.required) return 'Archivo requerido';
         }
       }
     } else {
-      if (rules.required) return 'Campo requerido';
+      if (rules.required) return 'Respuesta requerida';
     }
   }
   return true;
